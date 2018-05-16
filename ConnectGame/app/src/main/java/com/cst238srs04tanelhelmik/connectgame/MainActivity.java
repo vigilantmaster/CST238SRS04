3aea6b13f9bc034218581c7d8ae970a1f52c57a0
Tanel Helmik <vigilantmaster@hotmail.com>
2018-05-15 19:08:30 -0700

    adding to the repository my app

---
+++ ConnectGame/app/src/main/java/com/cst238srs04tanelhelmik/connectgame/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cst238srs04tanelhelmik.connectgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
